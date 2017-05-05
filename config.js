eb2c1f70e2f8fdce09fa1e74096efbe1cbb2bac4
Nico_Zhang <2091571762@qq.com>
2017-05-05 16:09:24 +0800

    直连redis

---
+++ config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by nico on 2017/3/11.
+ */
+exports.ding = {
+    corpId:"",
+    secret:"",
+    prefix:"ding_"
+};
+exports.redis = {
+    port:6379,
+    host :'192.168.1.218',
+    auth:{}
+};
